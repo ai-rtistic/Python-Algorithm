2a12b9e04e9b65be8f64faf060164fb4e469ec77
Jaejun Lee <84179578+ai-rtistic@users.noreply.github.com>
2022-04-08 11:57:55 +0900

    first commit

---
+++ Alg03_DFS_BFS/quiz2.py
@@ -0,0 +1,39 @@
+from collections import deque
+
+
+
+def bfs(graph, x, y):
+    queue = deque()
+    queue.append((x,y))
+
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                continue
+
+            if graph[nx][ny]==0:
+                continue
+
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx,ny))
+
+    return graph[n-1][m-1]
+
+
+
+
+n,m = map(int, input().split())
+
+graphs = []
+for i in range(n):
+    graphs.append(list(map(int, input())))
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+print(bfs(graphs, 0, 0))
